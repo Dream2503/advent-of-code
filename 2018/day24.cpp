ff20b3f482a45223b3920a48a5f0fc8610325820
Dream <swapnarajmohanty2503@gmail.com>
2025-12-27 16:25:55 +0530

    commit

--- 2018/day24.cpp
+++ 2018/day24.cpp
@@ -238,54 +238,52 @@ void extract_defence(std::stringstream& ss, Group& group, char& end) {
 }
 
 int compute_effective_damage(const Group& attacker, const Group& defender) {
-    if (std::ranges::contains(defender.immune, attacker.attack)) {
-        return 0;
+    if (!std::ranges::contains(defender.immune, attacker.attack)) {
+        return attacker.units * attacker.damage * (std::ranges::contains(defender.weak, attacker.attack) ? 2 : 1);
     }
-    return attacker.units * attacker.damage * (std::ranges::contains(defender.weak, attacker.attack) ? 2 : 1);
+    return 0;
 }
 
-int part1() {
+std::vector<Group> parse_input(const char* input) {
     bool on_immune = false;
     int id = 0;
     std::string line;
     std::vector<Group> groups;
-    std::stringstream file(input24);
+    std::stringstream file(input);
 
     while (std::getline(file, line)) {
-        if (line.empty()) {
-            continue;
-        }
         if (line == "Immune System:") {
             on_immune = true;
-            continue;
-        }
-        if (line == "Infection:") {
+        } else if (line == "Infection:") {
             on_immune = false;
-            continue;
-        }
-        Group group;
-        std::stringstream ss(line);
-        ((ss >> group.units).ignore(17) >> group.hit_points).ignore(12);
+        } else if (!line.empty()) {
+            Group group;
+            std::stringstream ss(line);
+            ((ss >> group.units).ignore(17) >> group.hit_points).ignore(12);
 
-        if (ss.peek() == '(') {
-            char end;
-            ss.ignore(1);
-            extract_defence(ss, group, end);
-
-            if (end != ')') {
+            if (ss.peek() == '(') {
+                char end;
+                ss.ignore(1);
                 extract_defence(ss, group, end);
+
+                if (end != ')') {
+                    extract_defence(ss, group, end);
+                }
+                ss.ignore(1);
             }
-            ss.ignore(1);
+            std::string attack;
+            (ss.ignore(25) >> group.damage >> attack).ignore(22) >> group.initiative;
+            group.attack = to_attack(attack);
+            group.army = on_immune ? Army::IMMUNE_SYSTEM : Army::INFECTION;
+            group.id = id++;
+            groups.push_back(group);
         }
-        std::string attack;
-        (ss.ignore(25) >> group.damage >> attack).ignore(22) >> group.initiative;
-        group.attack = to_attack(attack);
-        group.army = on_immune ? Army::IMMUNE_SYSTEM : Army::INFECTION;
-        group.id = id++;
-        groups.push_back(group);
     }
-    while (std::ranges::count_if(groups, [](const Group& group) -> bool { return group.army == Army::IMMUNE_SYSTEM; }) &&
-           std::ranges::count_if(groups, [](const Group& group) -> bool { return group.army == Army::INFECTION; })) {
+    return groups;
+}
+
+Army simulate(std::vector<Group>& groups) {
+    while (std::ranges::count(groups, Army::IMMUNE_SYSTEM, &Group::army) && std::ranges::count(groups, Army::INFECTION, &Group::army)) {
         for (Group& group : groups) {
             group.selected = false;
             group.target = -1;
@@ -311,6 +309,7 @@ int part1() {
                 attacker.target = target->id;
             }
         }
+        bool active = false;
         std::ranges::sort(groups, std::ranges::greater(), &Group::initiative);
 
         for (Group& attacker : groups) {
@@ -318,20 +317,187 @@ int part1() {
                 auto defender = std::ranges::find(groups, attacker.target, &Group::id);
 
                 if (defender->units > 0) {
-                    defender->units -= compute_effective_damage(attacker, *defender) / defender->hit_points;
+                    const int killed = compute_effective_damage(attacker, *defender) / defender->hit_points;
+
+                    if (killed) {
+                        defender->units -= killed;
+                        active = true;
+                    }
                 }
             }
         }
+        if (!active) {
+            return Army::INFECTION;
+        }
         std::erase_if(groups, [](const Group& group) -> bool { return group.units <= 0; });
     }
+    return std::ranges::contains(groups, Army::IMMUNE_SYSTEM, &Group::army) ? Army::IMMUNE_SYSTEM : Army::INFECTION;
+}
+
+int part1() {
+    std::vector<Group> groups = parse_input(input24);
+    simulate(groups);
     return std::transform_reduce(groups.begin(), groups.end(), 0, std::plus(), [](const Group& group) -> int { return group.units; });
 }
 
 /*
 --- Part Two ---
+Things aren't looking good for the reindeer. The man asks whether more milk and cookies would help you think.
+
+If only you could give the reindeer's immune system a boost, you might be able to change the outcome of the combat.
+
+A boost is an integer increase in immune system units' attack damage. For example, if you were to boost the above example's immune system's units by
+1570, the armies would instead look like this:
+Immune System:
+17 units each with 5390 hit points (weak to radiation, bludgeoning) with
+ an attack that does 6077 fire damage at initiative 2
+989 units each with 1274 hit points (immune to fire; weak to bludgeoning,
+ slashing) with an attack that does 1595 slashing damage at initiative 3
+
+Infection:
+801 units each with 4706 hit points (weak to radiation) with an attack
+ that does 116 bludgeoning damage at initiative 1
+4485 units each with 2961 hit points (immune to radiation; weak to fire,
+ cold) with an attack that does 12 slashing damage at initiative 4
+
+With this boost, the combat proceeds differently:
+Immune System:
+Group 2 contains 989 units
+Group 1 contains 17 units
+Infection:
+Group 1 contains 801 units
+Group 2 contains 4485 units
+
+Infection group 1 would deal defending group 2 185832 damage
+Infection group 1 would deal defending group 1 185832 damage
+Infection group 2 would deal defending group 1 53820 damage
+Immune System group 2 would deal defending group 1 1577455 damage
+Immune System group 2 would deal defending group 2 1577455 damage
+Immune System group 1 would deal defending group 2 206618 damage
+
+Infection group 2 attacks defending group 1, killing 9 units
+Immune System group 2 attacks defending group 1, killing 335 units
+Immune System group 1 attacks defending group 2, killing 32 units
+Infection group 1 attacks defending group 2, killing 84 units
+Immune System:
+Group 2 contains 905 units
+Group 1 contains 8 units
+Infection:
+Group 1 contains 466 units
+Group 2 contains 4453 units
+
+Infection group 1 would deal defending group 2 108112 damage
+Infection group 1 would deal defending group 1 108112 damage
+Infection group 2 would deal defending group 1 53436 damage
+Immune System group 2 would deal defending group 1 1443475 damage
+Immune System group 2 would deal defending group 2 1443475 damage
+Immune System group 1 would deal defending group 2 97232 damage
+
+Infection group 2 attacks defending group 1, killing 8 units
+Immune System group 2 attacks defending group 1, killing 306 units
+Infection group 1 attacks defending group 2, killing 29 units
+Immune System:
+Group 2 contains 876 units
+Infection:
+Group 2 contains 4453 units
+Group 1 contains 160 units
+
+Infection group 2 would deal defending group 2 106872 damage
+Immune System group 2 would deal defending group 2 1397220 damage
+Immune System group 2 would deal defending group 1 1397220 damage
+
+Infection group 2 attacks defending group 2, killing 83 units
+Immune System group 2 attacks defending group 2, killing 427 units
+
+After a few fights...
+Immune System:
+Group 2 contains 64 units
+Infection:
+Group 2 contains 214 units
+Group 1 contains 19 units
+
+Infection group 2 would deal defending group 2 5136 damage
+Immune System group 2 would deal defending group 2 102080 damage
+Immune System group 2 would deal defending group 1 102080 damage
+
+Infection group 2 attacks defending group 2, killing 4 units
+Immune System group 2 attacks defending group 2, killing 32 units
+Immune System:
+Group 2 contains 60 units
+Infection:
+Group 1 contains 19 units
+Group 2 contains 182 units
+
+Infection group 1 would deal defending group 2 4408 damage
+Immune System group 2 would deal defending group 1 95700 damage
+Immune System group 2 would deal defending group 2 95700 damage
+
+Immune System group 2 attacks defending group 1, killing 19 units
+Immune System:
+Group 2 contains 60 units
+Infection:
+Group 2 contains 182 units
+
+Infection group 2 would deal defending group 2 4368 damage
+Immune System group 2 would deal defending group 2 95700 damage
+
+Infection group 2 attacks defending group 2, killing 3 units
+Immune System group 2 attacks defending group 2, killing 30 units
+
+After a few more fights...
+Immune System:
+Group 2 contains 51 units
+Infection:
+Group 2 contains 40 units
+
+Infection group 2 would deal defending group 2 960 damage
+Immune System group 2 would deal defending group 2 81345 damage
+
+Infection group 2 attacks defending group 2, killing 0 units
+Immune System group 2 attacks defending group 2, killing 27 units
+Immune System:
+Group 2 contains 51 units
+Infection:
+Group 2 contains 13 units
+
+Infection group 2 would deal defending group 2 312 damage
+Immune System group 2 would deal defending group 2 81345 damage
+
+Infection group 2 attacks defending group 2, killing 0 units
+Immune System group 2 attacks defending group 2, killing 13 units
+Immune System:
+Group 2 contains 51 units
+Infection:
+No groups remain.
+
+This boost would allow the immune system's armies to win! It would be left with 51 units.
+
+You don't even know how you could boost the reindeer's immune system or what effect it might have, so you need to be cautious and find the smallest
+boost that would allow the immune system to win.
+
+How many units does the immune system have left after getting the smallest boost it needs to win?
 */
 
-int part2() { return 0; }
+int part2() {
+    const std::vector<Group> groups = parse_input(input24);
+    int buff = 1;
+    std::vector<Group> temp;
+    temp.reserve(groups.size());
+
+    while (simulate(temp) != Army::IMMUNE_SYSTEM) {
+        temp.clear();
+
+        for (const Group& group : groups) {
+            temp.push_back(group);
+
+            if (group.army == Army::IMMUNE_SYSTEM) {
+                temp.back().damage += buff;
+            }
+        }
+        buff++;
+    }
+    return std::transform_reduce(temp.begin(), temp.end(), 0, std::plus(), [](const Group& group) -> int { return group.units; });
+}
 
 int main() {
     std::cout << part1() << std::endl << part2() << std::endl;
