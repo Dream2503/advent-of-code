9d65791c3d636e3e99a9cfb422ecd68b21af192b
Dream <swapnarajmohanty2503@gmail.com>
2025-12-27 18:03:01 +0530

    commit

---
+++ 2019/inputs.hpp
@@ -0,0 +1,151 @@
+#pragma once
+#include "../utils.hpp"
+
+constexpr char input1[] = R"(137569
+146535
+74662
+133844
+99969
+86606
+76237
+52902
+106211
+141865
+50865
+101011
+75956
+67501
+142146
+107706
+83492
+137253
+56296
+141256
+118232
+127402
+67455
+64062
+72416
+109547
+106144
+54832
+57057
+74884
+80923
+85121
+60461
+92743
+134175
+65671
+90198
+134055
+59568
+146576
+134488
+130355
+54782
+51370
+55501
+56555
+62140
+99558
+80875
+113451
+71048
+64890
+94481
+87468
+148972
+81742
+79471
+100999
+106741
+142433
+130225
+58789
+134365
+81310
+102004
+92736
+105542
+63097
+92747
+109214
+103305
+143659
+68254
+126409
+71724
+50284
+125431
+132227
+125600
+99131
+96598
+101007
+123104
+82215
+97310
+135824
+117379
+81546
+109472
+85571
+89292
+109530
+127656
+56654
+132463
+101948
+118835
+59125
+116089
+61605)";
+
+constexpr char input2[] = R"()";
+
+constexpr char input3[] = R"()";
+
+constexpr char input4[] = R"()";
+
+constexpr char input5[] = R"()";
+
+constexpr char input6[] = R"()";
+
+constexpr char input7[] = R"()";
+
+constexpr char input8[] = R"()";
+
+constexpr char input9[] = R"()";
+
+constexpr char input10[] = R"()";
+
+constexpr char input11[] = R"()";
+
+constexpr char input12[] = R"()";
+
+constexpr char input13[] = R"()";
+
+constexpr char input14[] = R"()";
+
+constexpr char input15[] = R"()";
+
+constexpr char input16[] = R"()";
+
+constexpr char input17[] = R"()";
+
+constexpr char input18[] = R"()";
+
+constexpr char input19[] = R"()";
+
+constexpr char input20[] = R"()";
+
+constexpr char input21[] = R"()";
+
+constexpr char input22[] = R"()";
+
+constexpr char input23[] = R"()";
+
+constexpr char input24[] = R"()";
+
+constexpr char input25[] = R"()";
